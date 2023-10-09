8d00584959690668a039a8502df0ebe8e5e654b9
CrowerTP <t.peter222@gmail.com>
2023-10-10 00:13:47 +0200

    Finished the mongoose module

--- Databases/FruitsProject/clients.js
+++ Databases/FruitsProject/clients.js
@@ -1,23 +1,88 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://localhost:27017/clientsDB";
+const uri = "mongodb://127.0.0.1:27017/clientsDB";
 
 dbConnect().catch(err => console.log(err));
 
 async function dbConnect() {
-    await mongoose.connect(uri);
-}
+    try {
+        await mongoose.connect(uri);
+    } catch (error) {
+        console.error("Failed to connect: " + error);
+    }
+
+};
+
+const productSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    stock: Number,
+    reviews: Array
+});
+
+const Product = mongoose.model("Product", productSchema);
+const product = new Product({
+    name: "Pencil",
+    price: 0.3,
+    stock: 12
+});
+
+//saveDB(product);
 
 const clientsSchema = mongoose.Schema({
-    name:   String,
-    age:    Number
-})
+    name: {
+        type: String,
+        required: [true, "Name has to be defined"]
+    },
+    age: Number,
+    favouriteProduct: productSchema
+});
 
 const Client = mongoose.model("Client", clientsSchema);
-
 const client = new Client({
-    name: "John",
-    age: 32
-})
+    name: "Anna",
+    age: 28,
+    favouriteProduct: product
+});
 
-client.save();
+
+//saveDB(client);
+
+
+//updateDB();
+//deleteDB();
+
+
+// const clients = await Product.find();
+
+// clients.forEach(doc => {
+//     console.log(doc.name);
+// });
+
+
+async function saveDB(db){
+    try {
+        await db.save();
+        console.log("Save was successful!")
+    } catch (error) {
+        console.error("Failed to save the db: " + error);
+    }
+};
+
+async function updateDB(){
+    try {
+        await Client.updateOne({name: "Peter"}, {favouriteProduct: product});
+        console.log("Updating document was successful!");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+async function deleteDB(){
+    try {
+        await Product.deleteOne({_id: "652476067f27f3269baea812"});
+        console.log("Delete was succesful!")
+    } catch (error) {
+        console.log("Failed to delete: " + error);
+    }
+};
