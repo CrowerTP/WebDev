c588d9a3cdb844690407e89ca2f20affeba0b72a
CrowerTP <t.peter222@gmail.com>
2024-01-04 01:42:35 +0100

    prettier

--- javascript/Fundamentals_part2/index.js
+++ javascript/Fundamentals_part2/index.js
@@ -1,22 +1,23 @@
-'use strict';
-
+"use strict";
 
 const peter = {
-    firstName: 'Péter',
-    lastName: 'Telek',
-    birthYear: 1992,
-    job: 'Engineer',
-    favouriteAnimal: 'Elephant',
-    friends: ['Laci', 'Norbi', 'Máté'],
+  firstName: "Péter",
+  lastName: "Telek",
+  birthYear: 1992,
+  job: "Engineer",
+  favouriteAnimal: "Elephant",
+  friends: ["Laci", "Norbi", "Máté"],
 
-    calcAge: function(){
-        return this.age = 2024-this.birthYear;
-    },
-    hasDriversLicence:  true,
-    getSummary: function(){
-        const Summary = `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and has ${this.hasDriversLicence ? 'a' : 'no'} Drivers Licence.`;
-        return Summary;
-    }
+  calcAge: function () {
+    return (this.age = 2024 - this.birthYear);
+  },
+  hasDriversLicence: true,
+  getSummary: function () {
+    const Summary = `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and has ${this.hasDriversLicence ? "a" : "no"} Drivers Licence.`;
+    return Summary;
+  },
 };
 
 console.log(peter.getSummary());
