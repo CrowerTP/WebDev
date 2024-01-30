530b4743f0096f9bd9b5a9a65193378ce5fb14cd
CrowerTP <t.peter222@gmail.com>
2024-01-31 00:21:28 +0100

    learn scoping

--- javascript/Scoping/index.js
+++ javascript/Scoping/index.js
@@ -1,2 +1,24 @@
 "use strict";
 
+function calcAge(birthYear){
+    const age = 2024-birthYear;
+    
+    function printAge() {
+        const output = `${firstName} are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1992 && birthYear <= 1996){
+            var millenial = true;
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log (str);
+        } 
+        // console.log(str);
+        console.log(millenial);
+    }
+    printAge();
+
+    return age
+}
+
+const firstName = 'Peter';
+calcAge(1992);
