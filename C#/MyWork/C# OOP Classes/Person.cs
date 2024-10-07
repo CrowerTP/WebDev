0c594c46898c4a381d2eb53c35be3e7f2b3d746d
Peter Telek <peter.telek@evosoft.com>
2024-10-07 13:24:33 +0200

    c# intermediate

---
+++ C#/MyWork/C# OOP Classes/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__OOP_Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hello {0}, my name is {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
---
+++ C#/MyWork/C# OOP Classes/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using C__OOP_Classes;
+
+var peti = Person.Parse("Péter");
+peti.Introduce("Kérdező");
